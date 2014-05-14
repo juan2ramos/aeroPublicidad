e6c0df86ec192f0dc8243ac978fa5f1359cd1495
juan2ramos <juan2ramos@gmail.com>
2014-05-14 14:40:56 -0500

    + Estructura plantilla

---
+++ layout/default.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="es">
+<head>
+
+    <title><?php echo $title ?></title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="author" content="juan2ramos"/>
+    <meta name="contact" content="juan2ramos@gmail.com"/>
+    <meta name="description" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
+    <!-- Meta Facebook -->
+    <meta property="og:url" content="http://juan2ramos.com/">
+    <meta property="og:title" content="Juan2ramos"/>
+    <meta property="og:description" content="juan2ramos"/>
+    <meta property="og:image" content="images/logo.png"/>
+
+
+    <link rel="shortcut icon" href="favicon.png">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic'
+          rel='stylesheet' type='text/css'>
+
+    <!-- Estilos -->
+    <script src="js/prefixfree.min.js"></script>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+
+
+</head>
+<body>
+<header>
+
+    <nav>
+        <ul>
+            <li></li>
+        </ul>
+
+
+    </nav>
+</header>
+
+
+
+<?php include $fileName; ?>
+
+<footer>
+    <ul id="nav-footer">
+        <li></li>
+    </ul>
+</footer>
+<!-- JavaScript -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
